478d4e999dea44e38f91290e18e42f55a2affe50
danicelistobon <danielct-97@hotmail.com>
2020-01-27 22:52:12 +0000

    4. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module to start an API
+"""
+from models import storage
+from api.v1.views import app_views
+from flask import Flask
+from os import getenv
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """calls storage.close()
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST') or '0.0.0.0'
+    port = getenv('HBNB_API_PORT') or 5000
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Index file
+"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route("/status")
+def status():
+    """returns a JSON: "status": "OK"
+    """
+    dict_obj = {"status": "OK"}
+    return jsonify(dict_obj)
