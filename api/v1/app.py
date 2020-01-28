1503b1b9092a750951197154be8786403cc5a18d
Liliana327 <894@holbertonschool.com>
2020-01-28 19:58:08 +0000

    6. Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from os import getenv
 
 
@@ -18,6 +18,12 @@ def teardown_appcontext(self):
     """
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """handler for 404 errors that returns a JSON-formatted 404
+    """
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST') or '0.0.0.0'
