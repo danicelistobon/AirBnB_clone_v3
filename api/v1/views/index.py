8817762321ac4d8fa9add0d546cfed949ff70f08
danicelistobon <danielct-97@hotmail.com>
2020-01-29 02:54:51 +0000

    5. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -18,10 +18,10 @@ def status():
 def count():
     """endpoint that retrieves the number of each objects by type
     """
-    dir_number = { "amenities": storage.count("amenities"),
-                   "cities": storage.count("cities"),
-                   "places": storage.count("places"),
-                   "reviews": storage.count("reviews"),
-                   "states": storage.count("states"),
-                   "users": storage.count("users")}
+    dir_number = { "amenities": storage.count("Amenity"),
+                   "cities": storage.count("City"),
+                   "places": storage.count("Place"),
+                   "reviews": storage.count("Review"),
+                   "states": storage.count("State"),
+                   "users": storage.count("User")}
     return jsonify(dir_number)
